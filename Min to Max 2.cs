3fcfeac20a71b717fabf456f82f5550a4f9296dc
GFStealer-666 <naroj.joi@mail.kmutt.ac.th>
2021-06-13 17:20:23 +0700

    ข้อ 2

---
+++ Min to Max 2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace MIn_to_Max2
+{
+    class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                Console.Write("How many number you want? : ");
+                int x = int.Parse(Console.ReadLine());
+
+                int[] Number1 = new int[x];
+
+                for (int i = 0; i < x; i++)
+                {
+                    Console.Write("Number : ");
+                    Number1[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Mim to Max sort : ");
+                for (int i = 0; i < x; i++)
+                {
+
+                    Array.Sort(Number1);
+                    Console.Write("{0}  ", Number1[i]);
+                }
+                Console.WriteLine("");
+
+                Console.WriteLine("Ez sort : ");
+                for (int i = 0; i < x; i++)
+                {
+                    Array.Sort(Number1);
+                    Array.Reverse(Number1);
+                    Array.Sort(Number1);
+                    Console.Write("{0}  ", Number1[i]);
+                }
+
+
+                Console.WriteLine("");
+
+                Console.WriteLine("2EZ sort : ");
+                for (int i = 0; i < x; i++)
+                {
+                    Array.Sort(Number1);
+                    Array.Reverse(Number1);
+                    Array.Sort(Number1);
+                    Array.Reverse(Number1);
+                    Array.Sort(Number1);
+                    Console.Write("{0}  ", Number1[i]);
+                }
+                Console.WriteLine("");
+
+                Console.WriteLine("2EZ4ME sort : ");
+                for (int i = 0; i < x; i++)
+                {
+                    Array.Sort(Number1);
+                    Array.Reverse(Number1);
+                    Array.Sort(Number1);
+                    Array.Reverse(Number1);
+                    Array.Sort(Number1);
+                    Array.Reverse(Number1);
+                    Array.Sort(Number1);
+                    Console.Write("{0}  ", Number1[i]);
+
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+
---
+++ Min to max.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Min_to_Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many number you want? : ");
+             int x = int.Parse(Console.ReadLine());
+
+            int[] Number1 = new int[x];
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("Number : ");
+                Number1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Mim to Max sort : ");
+            for (int i =0; i < x; i++)
+            {
+                
+                Array.Sort(Number1);
+                Console.Write("{0} , ",Number1[i]);                
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Max to Min sort : ");
+            for (int i = 0; i < x; i++)
+            {               
+                Array.Sort(Number1);
+                Array.Reverse(Number1);
+                Console.Write("{0} , ", Number1[i]);
+            }
+
+            int[] Number2 = new int[x];
+
+            for(int i =0; i <x; i++)
+            {
+               Number2[i] = Math.Abs(Number1[i]);
+
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Abs Min to Max sort : ");
+            for (int i = 0; i < x; i++)
+            {
+                Array.Sort(Number2);
+                Console.Write("{0} , ", Number2[i]);
+
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Abs Max to Min sort : ");
+            for (int i = 0; i < x; i++)
+            {
+                Array.Sort(Number2);
+                Array.Reverse(Number2);
+                Console.Write("{0} , ", Number2[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
